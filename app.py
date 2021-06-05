f8a217197c7657576b55c5e31eb4f9ce8eb8179b
Cyd-Styles <cydhaynes0@gmail.com>
2021-06-05 13:28:47 -0400

    Updating all code so that everyone is working

--- app.py
+++ app.py
@@ -13,5 +13,33 @@ scheduler.start()
 def add_tasks():
     return render_template('home.html')
 
+# Store Journal Entry Form Inputs in DB
+@app.route('/send', methods=["POST"])
+def sent():
+    # Get Posted Data Using Names Assigned in HTML
+    title = request.form['journal-entry-title']
+    date = request.form['journal-entry-date']
+    entry = request.form['journal-entry-text']
+
+    # Connect to DB and Insert into DB Columns
+    conn.sqlite3.connect('./static/data/journal-entries.db')
+    curs = conn.cursor()
+    curs.execute("INSERT INTO entries (entry_titles, entry_dates, entries) VALUES ((?), (?), (?))", (title, date, entry))
+    conn.commit()
+
+    # Close DB Connection
+    conn.close()
+
+    # Render Template with Success Message Displaying What the User Entered
+    return render_template('added-entry.html', title = entry_titles, date = entry_dates, entry = entries)
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
