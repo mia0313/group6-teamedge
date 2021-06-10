a560e0b8a94759cfec8136c76fa768da24ed9933
Cyd-Styles <cydhaynes0@gmail.com>
2021-06-09 23:44:10 -0400

    completed db with no errors

--- app.py
+++ app.py
@@ -21,14 +21,14 @@ def display_entry():
     if request.method == 'POST':
         title = request.form['journal-entry-title']
         date = request.form['journal-entry-date']
-        entry = request.form['journal-entry-text']
+        text = request.form['journal-entry-text']
         conn = sqlite3.connect('./static/data/journal_entries.db')
         curs = conn.cursor()
-        curs.execute("INSERT INTO entries(entry_titles, entry_dates, entry_texts) VALUES ((?), (?), (?))", (title, date, entry))
+        curs.execute("INSERT INTO entries(entry_titles, entry_dates, entry_texts) VALUES ((?), (?), (?))", (title, date, text))
         conn.commit()
 
         conn.close()
-        return render_template('added-entry.html', title = entry_titles, date = entry_dates, entry = entry_texts)  
+        return render_template('added-entry.html', entry_titles = title, entry_dates = date, entry_texts = text)  
 
 
 
@@ -42,7 +42,7 @@ def all_entries():
 
         conn = sqlite3.connect('./static/data/journal_entries.db')
         curs = conn.cursor()
-        curs.execute("INSERT INTO user_entries1 (entry_titles, entry_dates, entry_texts) VALUES ((?), (?), (?))", (title, date, text))
+        curs.execute("INSERT INTO entries(entry_titles, entry_dates, entry_texts) VALUES ((?), (?), (?))", (title, date, text))
         conn.commit()
 
         conn.close()
@@ -50,28 +50,18 @@ def all_entries():
         conn =  sqlite3.connect('./static/data/journal_entries.db')
         curs = conn.cursor()
         entries = []
-        rows = curs.execute("SELECT * FROM user_entries1 ORDER BY entry_dates ASC;")
+        rows = curs.execute("SELECT * FROM entries ORDER BY entry_dates ASC;")
         
         for row in rows:
             entry = {'rowid': row[0], 'entry_titles': row[1], 'entry_dates': row[2], 'entry_texts': row[3]}
             entries.append(entry)
             print(entry)
         conn.close()
+        return render_template('completed-entries.html', entries = entries)    
+    
 
-        return render_template('added-entry.html', entry_titles = title, entry_dates = date, entry_texts = text)
 
-
-
-    conn =  sqlite3.connect('./static/data/journal_entries.db')
-    curs = conn.cursor()
-    entries = []
-    rows = curs.execute("SELECT * FROM entries ORDER BY entry_dates ASC;")
-        
-    for row in rows:
-        entry = {'rowid': row[0], 'entry_titles': row[1], 'entry_dates': row[2], 'entry_texts': row[3]}
-        entries.append(entry)
-    conn.close()
-    return render_template(completed-entries.html, entries = entries)    
+    
     
        
 if __name__ == '__main__':
