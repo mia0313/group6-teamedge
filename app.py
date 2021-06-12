201081796c086e000a9db8ac4ec9f35281d9f55d
Cyd-Styles <cydhaynes0@gmail.com>
2021-06-12 11:16:56 -0400

    Added datetime function

--- app.py
+++ app.py
@@ -13,11 +13,26 @@ scheduler.start()
 def add_entry():
     return render_template('home.html')
 
+def cleanDate(date):
+    format_entry_date = date.split('-')
+    year = format_entry_date[0]
+    month = format_entry_date[1]
+    day_time = format_entry_date[2].split('T')
+    day = day_time[0]
+    time = day_time[1]
+
+    months = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "August", "09": "September", "10": "October", "11": "November", "12": "December"}
+    return months[month] + " " + day + ',' + " " + year + " " + time
+
 # Journal Entries Display Page
 
 # Store Journal Entry Form Inputs in DB
 @app.route('/submitted', methods=["POST", "GET"])
 def display_entry():
+    clean_date = ""
+    title = ""
+    date = ""
+    text = ""
     if request.method == 'POST':
         title = request.form['journal-entry-title']
         date = request.form['journal-entry-date']
@@ -27,7 +42,8 @@ def display_entry():
         curs.execute("INSERT INTO entries(entry_titles, entry_dates, entry_texts) VALUES ((?), (?), (?))", (title, date, text))
         conn.commit()
         conn.close()
-    return render_template('added-entry.html', entry_titles = title, entry_dates = date, entry_texts = text)  
+        clean_date = cleanDate(date)
+    return render_template('added-entry.html', entry_titles = title, entry_dates = clean_date, entry_texts = text)  
     
 @app.route('/all-entries', methods=["GET"])
 def all_entries():
@@ -43,16 +59,6 @@ def all_entries():
     return render_template('completed-entries.html', entries = entries)    
     
 
-def cleanDate(date):
-    format_entry_date = date.split('-')
-    year = format_entry_date[0]
-    month = format_entry_date[1]
-    day_time = format_entry_date[2].split('T')
-    day = day_time[0]
-    time = day_time[1]
-
-    months = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "August", "09": "September", "10": "October", "11": "November", "12": "December"}
-    return months[month] + day + ',' + year + " " + time
 
 
 if __name__ == '__main__':
